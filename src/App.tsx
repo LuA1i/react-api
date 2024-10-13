4e42fc0a068d3d342862fabd4666ca2ffacdb47e
Luqman Ali <luqman.ali270304@outlook.com>
2024-10-13 10:39:32 +0100

    initail commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
+const BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+interface Post {
+  id: number
+  title: string
+}
 function App() {
+  const [posts, setPosts] = useState<Post[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState()
+
+  useEffect(() => {
+    setIsLoading(true)
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}/posts`)
+        const posts = (await response.json()) as Post[]
+        setPosts(posts)
+      } catch (e: any) {
+        setError(e)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchPosts()
+  }, [])
+
+  if (isLoading) {
+    return <h1>Posts is Loading</h1>
+  }
+
+  if (error) {
+    return <h1>Something went wrong please try again</h1>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>DAta Fetching In React</h1>
+      <ul>
+        {posts.map((post) => {
+          return <li key={post.id}>{post.title}</li>
+        })}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
